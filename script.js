b39432586e6cb3ba62f747eabd5b4977e2c7a8c8
0xromrom <roman0811@live.nl>
2022-07-13 14:19:03 -0700

    buttons added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const line1 = document.querySelector(".line1");
 const line2 = document.querySelector(".line2");
 const line3 = document.querySelector(".line3");
 const textBox = document.querySelector(".header-text-box");
+const buttonBox = document.querySelector(".button-container");
 
 setTimeout(() => {
   textBox.classList.remove("hidden");
@@ -23,3 +24,7 @@ setTimeout(() => {
   line3.classList.remove("hidden");
   line3.classList.add("line-fade");
 }, 13500);
+
+setTimeout(() => {
+  buttonBox.classList.remove("hidden");
+}, 16000);
