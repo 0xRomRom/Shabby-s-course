d189d2eba79091de385b1f4ea0b35761d94ed7d4
0xromrom <roman0811@live.nl>
2022-07-13 16:40:55 -0700

    bootscreen

--- script.js
+++ script.js
@@ -5,26 +5,51 @@ const line2 = document.querySelector(".line2");
 const line3 = document.querySelector(".line3");
 const textBox = document.querySelector(".header-text-box");
 const buttonBox = document.querySelector(".button-container");
+const loadingText = document.querySelector(".loadingtext");
+const bootScreen = document.querySelector(".bootscreen");
+
+//Bootscreen animations
+
+(function () {
+  loadingText.insertAdjacentHTML("beforeend", " DeFi");
+  setTimeout(() => {
+    loadingText.textContent = "Loading: ";
+    loadingText.insertAdjacentHTML("beforeend", " Airdrops");
+  }, 1000);
+  setTimeout(() => {
+    loadingText.textContent = "Loading: ";
+    loadingText.insertAdjacentHTML("beforeend", " Financial Independence");
+  }, 2000);
+  setTimeout(() => {
+    loadingText.textContent = "Loading: ";
+    loadingText.insertAdjacentHTML("beforeend", " Gateway To Freedom");
+  }, 4000);
+  setTimeout(() => {
+    bootScreen.classList.add("hidden");
+  }, 5000);
+})();
+
+//Homescreen animations
 
 setTimeout(() => {
   textBox.classList.remove("hidden");
-}, 3500);
+}, 11000);
 
 setTimeout(() => {
   line1.classList.remove("hidden");
   line1.classList.add("line-fade");
-}, 4500);
+}, 13000);
 
 setTimeout(() => {
   line2.classList.remove("hidden");
   line2.classList.add("line-fade");
-}, 8500);
+}, 17500);
 
 setTimeout(() => {
   line3.classList.remove("hidden");
   line3.classList.add("line-fade");
-}, 13500);
+}, 22000);
 
 setTimeout(() => {
   buttonBox.classList.remove("hidden");
-}, 16000);
+}, 25000);
